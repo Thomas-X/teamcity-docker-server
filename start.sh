7926e066cac72278e2d97e0133260d404f4fb7b2
Thomas Zwarts <thomaszwarts@gmail.com>
2019-12-08 22:19:12 +0100

    remove teamcity

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
+mkdir dist
+wget -o teamcity.tar.gz https://download-cf.jetbrains.com/teamcity/TeamCity-2019.1.5.tar.gz --show-progress
+tar zxf teamcity.tar.gz -C dist/
+rm -rf teamcity.tar.gz
 docker image build -t teamcitypi:latest .
 docker container run --name teamcitypi:latest
